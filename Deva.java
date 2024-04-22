825bb0799a6edacd39c9ac76bec52fe2bfe43f4d
devaanirudh <devaanirudh323@gmail.com>
2024-04-22 15:42:29 +0530

    added AppDevProgram1

---
+++ Deva.java
@@ -0,0 +1,12 @@
+public class Deva{
+  public static void main(String[] args) {
+    // int x = 100;
+    // if (x % 2 != 0) {
+    //   System.out.println("Odd number");
+    // }
+    // else{
+    //   System.out.println("Even number");
+    // }
+    System.out.println("Hello world");
+  }
+}
